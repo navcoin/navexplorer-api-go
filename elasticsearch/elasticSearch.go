cf048ffd7d260cb34dc10bbdd003c4a13ed20d9c
dantudor <dtudor01@gmail.com>
2019-01-16 08:46:46 +0000

    Elastic search returns err

--- elasticsearch/elasticSearch.go
+++ elasticsearch/elasticSearch.go
@@ -3,11 +3,12 @@ package elasticsearch
 import (
 	"github.com/NavExplorer/navexplorer-api-go/config"
 	"github.com/olivere/elastic"
+	"github.com/pkg/errors"
 	"log"
 	"os"
 )
 
-func NewClient() (client *elastic.Client) {
+func NewClient() (client *elastic.Client, err error) {
 	opts := []elastic.ClientOptionFunc{
 		elastic.SetURL(config.Get().ElasticSearch.Urls),
 		elastic.SetSniff(config.Get().ElasticSearch.Sniff),
@@ -15,15 +16,14 @@ func NewClient() (client *elastic.Client) {
 	}
 
 	if config.Get().Debug {
-		opts = append(opts, elastic.SetTraceLog(log.New(os.Stdout, "ELASTIC ", 0)))
+		opts = append(opts, elastic.SetTraceLog(log.New(os.Stdout, "", 0)))
 	}
 
-	client, err := elastic.NewClient(opts...)
-
+	client, err = elastic.NewClient(opts...)
 	if err != nil {
-		// Handle error
-		panic(err)
+		err = errors.New("Unable to connect to elastic search")
+		log.Print(err)
 	}
 
-	return client
+	return client, err
 }
