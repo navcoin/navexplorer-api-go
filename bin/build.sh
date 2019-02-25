1f9c7b161f58e448bccf77525c319e4dfee8fcdc
dantudor <dtudor01@gmail.com>
2019-02-25 14:51:49 +0000

    Delete build.sh

--- bin/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-export GOOS=linux
-export GOARG=amd64
-
-go build -o navexplorerApi
-
-docker build -t dantudor/navexplorer:navexplorerapi-0.0.1 .
-docker push dantudor/navexplorer:navexplorerapi-0.0.1
