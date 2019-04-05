80e4b7660d42ab9ecfc55fc13192119a574daa30
dantudor <dtudor01@gmail.com>
2019-04-05 10:14:07 +0100

    Adds lastSeen and stale to network/node

--- service/network/model.go
+++ service/network/model.go
@@ -6,6 +6,7 @@ type Node struct {
 	Address          string    `json:"address"`
 	Good             bool      `json:"good"`
 	LastSuccess      time.Time `json:"lastSuccess"`
+	LastSeen         time.Time `json:"lastSeen"`
 	Percent2h        float64   `json:"percent2h"`
 	Percent8h        float64   `json:"percent8h"`
 	Percent1d        float64   `json:"percent1d"`
@@ -16,4 +17,5 @@ type Node struct {
 	Version          string    `json:"version"`
 	UserAgent        string    `json:"userAgent"`
 	UserAgentVersion string    `json:"userAgentVersion"`
+	Stale            bool      `json:"stale"`
 }
