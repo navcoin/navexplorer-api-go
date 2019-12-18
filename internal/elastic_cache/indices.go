5c427c45e64a00f492875cf9b709d3e1bf7b973a
Dan Tudor <dtudor01@gmaiol.com>
2019-12-18 13:40:41 +0000

    Use elastic_cache indices

---
+++ internal/elastic_cache/indices.go
@@ -0,0 +1,39 @@
+package elastic_cache
+
+import (
+	"fmt"
+	"github.com/NavExplorer/navexplorer-api-go/internal/framework"
+)
+
+type Indices string
+
+var (
+	AddressIndex            Indices = "address"
+	AddressTransactionIndex Indices = "addresstransaction"
+	BlockIndex              Indices = "block"
+	BlockTransactionIndex   Indices = "blocktransaction"
+	ProposalIndex           Indices = "proposal"
+	DaoVoteIndex            Indices = "daovote"
+	PaymentRequestIndex     Indices = "paymentrequest"
+	SignalIndex             Indices = "signal"
+	SoftForkIndex           Indices = "softfork"
+)
+
+// Sets the network and returns the full string
+func (i *Indices) Get() string {
+	return fmt.Sprintf("%s.%s", framework.GetParameter("network", "mainnet"), string(*i))
+}
+
+func All() []Indices {
+	return []Indices{
+		AddressIndex,
+		AddressTransactionIndex,
+		BlockIndex,
+		BlockTransactionIndex,
+		ProposalIndex,
+		PaymentRequestIndex,
+		DaoVoteIndex,
+		SignalIndex,
+		SoftForkIndex,
+	}
+}
