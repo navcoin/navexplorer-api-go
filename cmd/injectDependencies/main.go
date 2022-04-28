ebccc83dc86fe7c8d06540fd56c13e9127907c96
dantudor <dtudor01@gmail.com>
2022-04-28 09:39:16 +0100

    upgrade dingo to v4

---
+++ cmd/injectDependencies/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"github.com/NavExplorer/navexplorer-api-go/v2/internal/config/di"
+	"github.com/sarulabs/dingo/v4"
+	"os"
+)
+
+func main() {
+	args := os.Args[1:]
+	if len(args) != 1 {
+		fmt.Println("usage: go run main.go path/to/output/directory")
+		os.Exit(1)
+	}
+
+	err := dingo.GenerateContainer((*di.Provider)(nil), args[0])
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+}
